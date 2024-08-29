9db2b6133f00bfb9c950f9f18a06c2db3ea9cd8c
VulturARG <luisbriones@gmail.com>
2024-08-29 11:34:27 -0300

    Remove unused code.

--- app/infrastructure/fastapi/routes.py
+++ app/infrastructure/fastapi/routes.py
@@ -2,12 +2,10 @@
 from fastapi import APIRouter
 
 from app.infrastructure.fastapi.notification_request import NotificationRequest
-from app.infrastructure.fastapi.router_handle_exceptions import handle_exceptions
 
 router = APIRouter()
 
 
 @router.post("/notification")
-@handle_exceptions()
 async def send_notification(notification: NotificationRequest):
     return {"status": "success", "data": notification}
