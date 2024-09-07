a3d849e104da2c1cb89fd46520efe7b0c3d0f921
VulturARG <luisbriones@gmail.com>
2024-09-07 17:22:01 -0300

    Use case added.

--- app/infrastructure/fastapi/routes.py
+++ app/infrastructure/fastapi/routes.py
@@ -1,9 +1,23 @@
+from app.domain.notifications.notification_category import NotificationCategory
+from app.domain.notifications.notification_dto import NotificationDTO
 from app.infrastructure.fastapi.notification_request import NotificationRequest
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
+
+from app.infrastructure.wirings.publish_message_use_case_wiring import PublishMessageUseCaseWiring
 
 router = APIRouter()
 
 
-@router.post("/notification")
+@router.post("/notification", status_code=status.HTTP_202_ACCEPTED)
 async def send_notification(notification: NotificationRequest):
-    return {"status": "success", "data": notification}
+    notification_dto = NotificationDTO(
+        category=NotificationCategory(notification.category),
+        message=notification.message,
+    )
+    publish_message_use_case = PublishMessageUseCaseWiring().instantiate()
+    publish_message_use_case.publish(notification=notification_dto)
+    return JSONResponse(
+        content={"status": "Notification received", "data": notification.model_dump()},
+        status_code=status.HTTP_202_ACCEPTED
+    )
