3ef11547b6b11dd2d26b8a153be24ec048246085
VulturARG <luisbriones@gmail.com>
2024-09-07 15:09:16 -0300

    Reload removed.

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from uvicorn import run
 from app.infrastructure.fastapi.fastapi_app import fastapi_app
 
 if __name__ == "__main__":
-    run(fastapi_app, host="0.0.0.0", port=8000, reload=True)
+    run(fastapi_app, host="0.0.0.0", port=8000)
