3557859f9bb3c329ead052e4b4604d745ad5ce2d
VulturARG <luisbriones@gmail.com>
2024-09-07 10:24:23 -0300

    test as_dict() method added.

---
+++ app/domain/tests/test_user_message_dto.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+
+from app.domain.database.user_entity import UserEntity
+from app.domain.gateways.message_dto import UserMessageDTO
+from app.domain.notifications.notification_channel import NotificationChannel
+
+
+class TestUserMessageDto(TestCase):
+    def test_as_dict(self):
+        expected = dict(
+            user=dict(
+                id="1",
+                name="John Doe",
+                email="john.doe@example.com",
+                phone="123456789",
+                channels=[NotificationChannel.SMS, NotificationChannel.EMAIL]
+            ),
+            message="Hello, World!"
+        )
+
+        user = UserEntity(
+            id="1",
+            name="John Doe",
+            email="john.doe@example.com",
+            phone="123456789",
+            channels=[NotificationChannel.SMS, NotificationChannel.EMAIL]
+        )
+
+        dto = UserMessageDTO(user=user, message="Hello, World!")
+        actual = dto.as_dict()
+        self.assertEqual(expected, actual)
