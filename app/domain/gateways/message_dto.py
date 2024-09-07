f3db2648270e5bfddea59726ed5cb23d7df89dda
VulturARG <luisbriones@gmail.com>
2024-09-07 10:16:39 -0300

    as_dict() method added.

--- app/domain/gateways/message_dto.py
+++ app/domain/gateways/message_dto.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+from typing import Any
 
 from app.domain.database.user_entity import UserEntity
 
@@ -9,3 +10,9 @@ class UserMessageDTO:
 
     user: UserEntity
     message: str
+
+    def as_dict(self) -> dict[str, Any]:
+        return {
+            "user": self.user.as_dict(),
+            "message": self.message
+        }
