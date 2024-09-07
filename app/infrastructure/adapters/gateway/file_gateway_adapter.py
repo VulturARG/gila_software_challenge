223b017e762385b51812d6ab6cb4a822744f9d5c
VulturARG <luisbriones@gmail.com>
2024-09-07 17:21:29 -0300

    Refactor variable name.

--- app/infrastructure/adapters/gateway/file_gateway_adapter.py
+++ app/infrastructure/adapters/gateway/file_gateway_adapter.py
@@ -4,12 +4,12 @@ from app.domain.gateways.file_port import FilePort
 class FileGatewayAdapter(FilePort):
     """I/O File Gateway adapter."""
 
-    def read(self, file_path_name: str) -> str:
+    def read(self, file_path: str) -> str:
         """Read the contents of the file."""
-        with open(file_path_name, "r") as file:
+        with open(file_path, "r") as file:
             return file.read()
 
-    def write(self, file_path_name: str, content: str) -> None:
+    def write(self, file_path: str, content: str) -> None:
         """Write the content to a file."""
-        with open(file_path_name, "w") as file:
-            file.write(content)
+        with open(file_path, "a") as file:
+            file.write(f"{content}\n")
