17aa5310e6a75f69a285be9354b5027bb58b92c5
VulturARG <luisbriones@gmail.com>
2024-09-08 10:32:48 -0300

    Remove unused param.

--- app/application/publish_message_use_case.py
+++ app/application/publish_message_use_case.py
@@ -1,13 +1,11 @@
 from app.domain.exceptions.decorators.generic_error_handler import generic_error_handler
-from app.domain.logs.log_service import LogService
 from app.domain.notifications.notification_dto import NotificationDTO
 from app.domain.notifications.notification_service import NotificationService
 
 
 class PublishMessageUseCase:
-    def __init__(self, notification_service: NotificationService, logger: LogService):
+    def __init__(self, notification_service: NotificationService):
         self._notification_service = notification_service
-        self._logger = logger
 
     @generic_error_handler
     def publish(self, notification: NotificationDTO):
